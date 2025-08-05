d9743b020e36e7ef336747192721a1fb61d7a92b
Ednaira Viana <ednairaviana.evs@gmail.com>
2025-08-05 00:54:41 -0300

    feat: Static Version

---
+++ src/components/Cv.jsx
@@ -0,0 +1,28 @@
+function Cv() {
+  return (
+    <div>
+      <div>
+        <h1>Mia Stone</h1>
+        <p>Ocupation</p>
+      </div>
+      <div>
+        <ul>
+          <li>mail@mail.com</li>
+          <li>123 12345678</li>
+          <li>@miastone</li>
+          <li>website.com</li>
+        </ul>
+        <div>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde
+            incidunt modi nisi, hic minima expedita, odio corporis esse
+            provident quos repellat possimus deleniti consequatur est amet,
+            error cumque itaque officiis!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cv;
---
+++ src/components/Inner.jsx
@@ -0,0 +1,15 @@
+import Header from "./Header";
+import Form from "./Form";
+import Cv from "./Cv";
+
+function Inner() {
+  return (
+    <>
+      <Header></Header>
+      <Form></Form>
+      <Cv></Cv>
+    </>
+  );
+}
+
+export default Inner;
