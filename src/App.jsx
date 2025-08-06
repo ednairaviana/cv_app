39ef57d87000698aecee0385f8f65098388dd047
Ednaira Viana <ednairaviana.evs@gmail.com>
2025-08-05 23:32:52 -0300

    feat [styles]: inner grid / main-nav / main-form

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import "./css/reset.css";
 import "./css/frame.css";
+import "./css/components/inner-grid.css";
+import "./css/components/main-nav.css";
+import "./css/components/main-form.css";
 import Inner from "./components/Inner";
 
 function App() {
