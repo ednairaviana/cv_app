90bba30d3d40205cf90683ab9c9bae888a634cb2
Ednaira Viana <ednairaviana.evs@gmail.com>
2025-08-10 23:11:39 -0300

    feat [styles] - First Part of CV

--- src/components/Cv.jsx
+++ src/components/Cv.jsx
@@ -1,24 +1,35 @@
 function Cv() {
   return (
-    <div>
-      <div>
-        <h1>Mia Stone</h1>
-        <p>Ocupation</p>
+    <div className="cv-model--1">
+      <div className="header">
+        <h1 className="title">Mia Stone</h1>
+        <p className="subtitle">Art Director</p>
       </div>
-      <div>
-        <ul>
-          <li>mail@mail.com</li>
-          <li>123 12345678</li>
-          <li>@miastone</li>
-          <li>website.com</li>
+      <div className="grid--2">
+        <ul className="info-list">
+          <li className="icon--before">
+            <span>mail@mail.com</span>
+          </li>
+          <li className="icon--before">
+            <span>123 12345678</span>
+          </li>
+          <li className="icon--before">
+            <span>Location</span>
+          </li>
+          <li className="icon--before">
+            <span>website.com</span>
+          </li>
         </ul>
         <div>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde
-            incidunt modi nisi, hic minima expedita, odio corporis esse
-            provident quos repellat possimus deleniti consequatur est amet,
-            error cumque itaque officiis!
-          </p>
+          <h2 className="subtitle">About Me</h2>
+          <div className="summary">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde
+              incidunt modi nisi, hic minima expedita, odio corporis esse
+              provident quos repellat possimus deleniti consequatur est amet,
+              error cumque itaque officiis!
+            </p>
+          </div>
         </div>
       </div>
     </div>
