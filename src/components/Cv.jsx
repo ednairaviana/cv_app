bca3732923d3e0a7c04a1b861a94203201196597
Ednaira Viana <ednaira@aldea.studio>
2025-11-28 22:43:23 -0300

    refactor: split form components

--- src/components/Cv.jsx
+++ src/components/Cv.jsx
@@ -1,6 +1,29 @@
+function ExperienceDividers({ all_xp_tabs }) {
+  console.log(all_xp_tabs);
+  return all_xp_tabs.map((tab) => {
+    return (
+      <div className="cv-divider" key={crypto.randomUUID()}>
+        <div className="cv-divider__header">
+          <h3 className="cv-divider__title">{tab.job_title}</h3>
+          <h4 className="cv-divider__sub-title">{tab.company}</h4>
+        </div>
+        <p className="cv-divider__tag">
+          <span>
+            {tab.start_date} - {tab.end_date}
+          </span>
+          <span> 1 year, {tab.location}</span>
+        </p>
+        <p className="cv-divider__description">{tab.description}</p>
+      </div>
+    );
+  });
+}
+
 function Cv({ cvData }) {
   const personal = cvData.general.personal;
   const contact = cvData.general.contact;
+  const social_media = cvData.general.social_media;
+  const experience_tabs = cvData.career.professional_experience;
 
   return (
     <div className="cv-model--1">
@@ -25,45 +48,16 @@ function Cv({ cvData }) {
           <span>{contact.location}</span>
         </li>
         <li className="icon--before">
-          <span>website.com</span>
+          <span>{social_media.website}</span>
         </li>
       </ul>
 
       <div className="cv-body">
         <div className="cv-section">
           <h2 className="cv-section__title">Experience</h2>
-          <div className="cv-divider">
-            <div className="cv-divider__header">
-              <h3 className="cv-divider__title">Art Director Senior</h3>
-              <h4 className="cv-divider__sub-title">Jelly Art</h4>
-            </div>
-            <p className="cv-divider__tag">
-              <span>Aug 18 - Present</span>
-              <span> 1 year, Santa Monica</span>
-            </p>
-            <p className="cv-divider__description">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde
-              incidunt modi nisi, hic minima expedita, odio corporis esse
-              provident quos repellat possimus deleniti consequatur est amet,
-              error cumque itaque officiis!
-            </p>
-          </div>
-          <div className="cv-divider">
-            <div className="cv-divider__header">
-              <h3 className="cv-divider__title">Art Director Senior</h3>
-              <h4 className="cv-divider__sub-title">Jelly Art</h4>
-            </div>
-            <p className="cv-divider__tag">
-              <span>Aug 18 - Present</span>
-              <span> 1 year, Santa Monica</span>
-            </p>
-            <p className="cv-divider__description">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde
-              incidunt modi nisi, hic minima expedita, odio corporis esse
-              provident quos repellat possimus deleniti consequatur est amet,
-              error cumque itaque officiis!
-            </p>
-          </div>
+          <ExperienceDividers
+            all_xp_tabs={experience_tabs}
+          ></ExperienceDividers>
         </div>
         <div className="cv-section">
           <h2 className="cv-section__title">Experience</h2>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -19,6 +19,61 @@ function Radio({ radioInfo }) {
   );
 }
 
+function Form({ tabs, useActiveTab, handleChangeFunction }) {
+  const activeTab = useActiveTab.activeTab;
+
+  return (
+    <div className="main-form_wrapper">
+      <form>
+        <Tabs
+          tabs={tabs}
+          activeTab={activeTab}
+          handleChangeFunction={handleChangeFunction}
+        ></Tabs>
+      </form>
+    </div>
+  );
+}
+
+function Tabs({ tabs, activeTab, handleChangeFunction }) {
+  return tabs.map((tab, index) => {
+    return (
+      <div
+        key={tab.id}
+        className={`main-form__tab ${activeTab === index ? "active" : ""}`}
+      >
+        <Fieldsets
+          fieldsets={tab.fieldsets}
+          tabId={tab.id}
+          handleChangeFunction={handleChangeFunction}
+        ></Fieldsets>
+      </div>
+    );
+  });
+}
+
+function Fieldsets({ fieldsets, tabId, handleChangeFunction }) {
+  return fieldsets.map((fieldset) => {
+    return (
+      <fieldset key={fieldset.id}>
+        <legend>{fieldset.legend}</legend>
+        <div className="main-form__fieldset-items">
+          {fieldset.fields.map((field) => (
+            <FieldWrapper
+              key={`${tabId}-${fieldset.id}-${field.id}`}
+              id={field.id}
+              type={field.type}
+              label={field.label}
+              cvPath={[tabId, fieldset.id, field.id]}
+              onCvChange={handleChangeFunction}
+            />
+          ))}
+        </div>
+      </fieldset>
+    );
+  });
+}
+
 function FieldWrapper(props) {
   const [isFilled, setIsFilled] = useState(false);
 
@@ -38,49 +93,6 @@ function FieldWrapper(props) {
           handleInputChange(e);
         }}
       />
-    </div>
-  );
-}
-
-function MainTab() {
-  return (
-    <div className="main-form__tab">
-      <fieldset></fieldset>
-    </div>
-  );
-}
-
-function Form({ tabs, useActiveTab, handleChangeFunction }) {
-  const activeTab = useActiveTab.activeTab;
-
-  return (
-    <div className="main-form_wrapper">
-      <form>
-        {tabs.map((tab, index) => (
-          <div
-            key={tab.id}
-            className={`main-form__tab ${activeTab === index ? "active" : ""}`}
-          >
-            {tab.fieldsets.map((fieldset, index) => (
-              <fieldset key={`${tab.id}-${fieldset.id}-${index}`}>
-                <legend>{fieldset.legend}</legend>
-                <div className="main-form__fieldset-items">
-                  {fieldset.fields.map((field) => (
-                    <FieldWrapper
-                      key={`${tab.id}-${fieldset.id}-${field.id}`}
-                      id={field.id}
-                      type={field.type}
-                      label={field.label}
-                      cvPath={[tab.id, fieldset.id, field.id]}
-                      onCvChange={handleChangeFunction}
-                    />
-                  ))}
-                </div>
-              </fieldset>
-            ))}
-          </div>
-        ))}
-      </form>
     </div>
   );
 }
